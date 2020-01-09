99f7989dd75b98151e38dd0380c3534daa736397
eshiett1995 <eshiett1995@gmail.com>
2020-01-09 18:06:06 +0100

    Initial commit

---
+++ src/main/java/com/gambeat/mimo/server/MimoSeverApplication.java
@@ -0,0 +1,13 @@
+package com.gambeat.mimo.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimoSeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimoSeverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gambeat/mimo/server/MimoSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gambeat.mimo.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimoSeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
