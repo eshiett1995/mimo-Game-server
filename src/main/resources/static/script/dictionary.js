1c64abbd6b7450e226979eeb4cc40fbd14dbe023
eshiett1995 <eshiett1995@gmail.com>
2020-01-14 17:51:39 +0100

    Front end for Gambeat

---
+++ src/main/resources/static/script/dictionary.js
@@ -0,0 +1,149 @@
+var dictionary = {
+    "welcome header": {
+        french: "Je suis un service de compétition en ligne social / numérique qui vous permet de rester connecté aux meilleurs jeux mobiles, de socialiser et de rivaliser entre plusieurs compétitions. Le jeu n'a jamais été aussi amusant et addictif. Votre histoire commence maintenant!",
+        english: "I am an online social/digital competition service that enables you stay connected to the best mobile games, socialize and compete accross various competitions. Gaming has never been this fun and addictive. Your story starts now!",
+        spanish: "Soy un servicio de competencia social / digital en línea que te permite estar conectado a los mejores juegos móviles, socializar y competir en varias competiciones. El juego nunca ha sido tan divertido y adictivo. ¡Tu historia comienza ahora!",
+        chinese: "我是一個在線社交/數字競賽服務，可讓您與各種比賽中的最佳手機遊戲，社交和競爭保持聯繫。遊戲從未如此有趣和令人上癮。你的故事現在開始"
+    },
+    "welcome note": {
+        french: "Je suis un service de compétition en ligne social / numérique qui vous permet de rester connecté aux meilleurs jeux mobiles, de socialiser et de rivaliser entre plusieurs compétitions. Le jeu n'a jamais été aussi amusant et addictif. Votre histoire commence maintenant!",
+        english: "I am an online social/digital competition service thats enables you stay connected to the best mobile games, socialize and compete accross various competitions. Gaming has never been this fun and addictive. Your story starts now!",
+        spanish: "Soy un servicio de competencia social / digital en línea que te permite estar conectado a los mejores juegos móviles, socializar y competir en varias competiciones. El juego nunca ha sido tan divertido y adictivo. ¡Tu historia comienza ahora!",
+        chinese: "我是一個在線社交/數字競賽服務，可讓您與各種比賽中的最佳手機遊戲，社交和競爭保持聯繫。遊戲從未如此有趣和令人上癮。你的故事現在開始"
+    },
+    "what we offer header": {
+        chinese: "我們提供什麼",
+        french:  "Ce que nous offrons",
+        spanish: "Lo que ofrecemos",
+        english: "What we offer"
+    },
+    "sign in button message": {
+        pt: "Descarregar plugin",
+        english: "Sign in"
+    },
+    "sign up button message": {
+        pt: "Descarregar plugin",
+        english: "Sign up"
+    },
+    "compete message": {
+        chinese: "參加大量比賽並獲得冠軍",
+        french:  "Participez à un grand nombre de jeux et soyez nommé champion",
+        spanish: "Compite en una gran cantidad de juegos y sé campeón",
+        english: "Compete on a vast number of games and be named champion"
+    },
+    "stay connected message": {
+        chinese: "在我們的平台上與玩家保持聯繫。",
+        french:  "Restez connecté avec les joueurs sur notre plateforme.",
+        spanish: "Mantente conectado con los jugadores en nuestra plataforma.",
+        english: "Stay connected with gamers on our platform."
+    },
+    "earn a living message": {
+        chinese: "遊戲不僅應該是有趣的，你可以從中獲益。",
+        french: "Le jeu ne doit pas seulement être amusant, vous pouvez en tirer profit.",
+        spanish: "El juego no solo debe ser divertido, sino que también puede beneficiarse de ello.",
+        english: "Gaming should not only be fun, you could gain from it."
+    },
+    "chat and socialize message": {
+        chinese: "在我們的平台上與各種遊戲玩家聊天和交流。",
+        french: "Discutez et socialisez avec différents joueurs sur notre plateforme.",
+        spanish: "Chatea y socializa con varios jugadores en nuestra plataforma.",
+        english: "Chat and socialize with various gamers on our platform."
+    },
+    "catalogue of games message": {
+        chinese: "我們有很多遊戲。",
+        french: "Nous avons un large catalogue de jeux.",
+        spanish: "Disponemos de un amplio catálogo de juegos.",
+        english: "We have a wide catalogue of games."
+    },
+    "how to play header": {
+        chinese: "怎麼玩",
+        french: "Comment jouer",
+        spanish: "Cómo jugar",
+        english: "How to play."
+    },
+    "contact us header": {
+        chinese: "聯繫我們",
+        french: "Contactez nous",
+        spanish: "Contáctenos",
+        english: "Contact us"
+    },
+    "contact us message": {
+        chinese: "我們喜歡聊天 - 畢竟我們確實製作了Messenger應用程序！您是否需要有關應用程序的幫助。\n" +
+        "\n" +
+        "對它的作用或需要任何其他信息有疑問\n" +
+        "\n" +
+        "請填寫下面的表格。您也可以給我們打電話或發電子郵件\n" +
+        "\n" +
+        "客戶服務代表每天24小時提供快速響應。",
+        french: "Nous aimons bavarder - nous avons néanmoins créé l'application de messagerie! Si vous avez besoin d'aide avec l'application.\n" +
+        "\n" +
+        "Vous avez une question sur ce qu'il fait ou besoin de toute autre information\n" +
+        "\n" +
+        "S'il vous plaît remplir le formulaire ci-dessous. Vous pouvez également nous téléphoner ou nous envoyer un courriel\n" +
+        "\n" +
+        "Les représentants du service clientèle sont disponibles 24 heures sur 24 pour répondre rapidement.",
+        spanish: "Nos encanta chatear, ¡hicimos la aplicación de mensajería después de todo! Si necesitas ayuda con la aplicación.\n" +
+        "\n" +
+        "Tiene una pregunta sobre lo que hace, o necesita cualquier otra información\n" +
+        "\n" +
+        "Por favor complete el siguiente formulario. También puede llamarnos o enviarnos un correo electrónico.\n" +
+        "\n" +
+        "Los representantes de servicio al cliente están disponibles las 24 horas del día para responder rápidamente.",
+        english: "We love to chat -we did make messenger app after all! Whether you need help with the app.\n" +
+        "\n" +
+        "    Have a question about what it does, or need any other information\n" +
+        "\n" +
+        "Please fill out the form bellow. You can also phone or e-mail us\n" +
+        "\n" +
+        "Customer service representatives are available 24 hours a day to respond quickly."
+    },
+    "submit button message": {
+        chinese: "提交",
+        french: "Soumettre",
+        spanish: "Enviar",
+        english: "Submit"
+    },
+
+    "login button message": {
+        chinese: "登錄",
+        french: "S'identifier",
+        spanish: "Iniciar sesión",
+        english: "Login"
+    },
+
+    "sign up button message": {
+        chinese: "註冊",
+        french: "S'inscrire",
+        spanish: "Regístrate",
+        english: "Sign up"
+    },
+
+    "home nav menu": {
+        chinese: "家",
+        french: "MAISON",
+        spanish: "CASA",
+        english: "HOME"
+    },
+
+    "what we offer nav menu": {
+        chinese: "我們提供什麼",
+        french: "CE QUE NOUS OFFRONS",
+        spanish: "LO QUE OFRECEMOS",
+        english: "WHAT WE OFFER"
+    },
+
+    "how to play nav menu": {
+        chinese: "怎麼玩",
+        french: "COMMENT JOUER",
+        spanish: "CÓMO JUGAR",
+        english: "HOW TO PLAY"
+    },
+
+    "contact us nav menu": {
+        chinese: "聯繫我們",
+        french: "CONTACTEZ NOUS",
+        spanish: "CONTÁCTENOS",
+        english: "CONTACT US"
+    },
+};
+
